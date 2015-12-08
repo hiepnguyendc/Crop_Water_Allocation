d0858acb9b050e93d50a87fd76b86022c189a97b
Duc Cong Hiep Nguyen <hiepndc@yahoo.com>
2015-12-08 20:06:10 +1030

    Update ACO_run.f90

--- ACO_run.f90
+++ ACO_run.f90
@@ -7,7 +7,6 @@
 
  subroutine ACO_run
    ! Aaron Zecchin, April 2003, modified by Joanna Szemis October 2010
-   ! modified by Duc Cong Hiep Nguyen, February 2013
    ! ACO_run performs all the required routines for a single run of ACO
       
 	use ACO_input
@@ -15,26 +14,24 @@
 	use ant_graph
    
 	integer :: n_it,n_ant,i,p,q
-	integer :: j,k,count,count2
+	integer :: j,k,count,count2, ioutput
 	integer :: l,r
     real(8) :: cuarea
     
 	call initialise_ACO_runtime_parameters
+   
+    ioutput = 1
    
 	do n_it = 1, max_it                    ! entering iteration loop        
  		do n_ant = 1, max_ant                 ! entering ant loop          
             ant(n_ant)%water_accumulated = 0.0
             do i=1,n_tree
                 do p = 1, n_sea
-                    sea_min_area(p) = sum(seasons(p)%min_crop_area(:))                    
                     ant(n_ant)%sea_cur_area(p) = 0
-                    ant(n_ant)%sea_cur_dryarea(p) = 0                    
-                    seasons(p)%wuse_crop(:) = 0
-                    
-                    do k = 1, n_crop(p)                 
-                        ant(n_ant)%tree(i)%season(p)%crop(k)%max_area = seasons(p)%max_crop_area(k)
+                    seasons(p)%wuse_crop(:) = -1.0
+                    do k = 1, n_crop(p) 
                         ant(n_ant)%tree(i)%season(p)%crop(k)%area_accumulated = 0
-                        ant(n_ant)%tree(i)%season(p)%crop(k)%area_planted = 0
+                        ant(n_ant)%tree(i)%season(p)%crop(k)%area_planted(:) = 0
                     end do
                 end do
                 
@@ -43,20 +40,22 @@
                     bstatus = 0
                     wbstatus = 0
                     do p = 1, n_sea
+                        call heuristic_crop(n_it,n_ant,i,q,1,p)
 					    call prob_determination(n_it,aco_type,i,n_ant,q,1,p)	! sets probabilities for ant loop
-                        call prob_determination_wateruse(n_it,aco_type,i,n_ant,q,1,p)
-                    end do
-                    do p = 1, n_sea
+					    ! there are two ways to determine probabilities for ant loop. here is the first one. the second one
+					    ! will calculate for the whole decision tree, meaning this subroutine will be run before this loop.
 					    call path_selection(n_it,n_ant,i,q,1,p)				    ! calling path selection subroutine                  
-                        if (seasons(p)%name_crop(ant(n_ant)%tree(i)%season(p)%dec_crop(q)) /= "dryland") then                            
+                        if (seasons(p)%name_crop(ant(n_ant)%tree(i)%season(p)%dec_crop(q)) /= "dryland") then
+                            call heuristic_wuse(n_it,n_ant,i,q,1,p)
+                            call prob_determination_wateruse(n_it,aco_type,i,n_ant,q,1,p)
                             call path_selection_wateruse(n_it,n_ant,i,q,1,p)                            
                         end if
                     end do
                 end do
-			end do
+			end do           
 			call path_evaluation(n_it, n_ant)
 		end do
-		call global_routines(n_it)  
+		call global_routines(n_it)
 	end do
   
 211 FORMAT(2x, I3, 2x, I3, 2x, I3, 2x, I3)
