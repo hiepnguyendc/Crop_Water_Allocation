e89a783e869c0cb612784145acdf382f3e05f18b
Duc Cong Hiep Nguyen <hiepndc@yahoo.com>
2015-12-08 19:53:37 +1030

    Update ACO_run.f90

--- ACO_run.f90
+++ ACO_run.f90
@@ -7,7 +7,7 @@
 
  subroutine ACO_run
    ! Aaron Zecchin, April 2003, modified by Joanna Szemis October 2010
-   ! modified by Duc Cong Hiep Nguyen, February 2013
+   ! modified by Duc Cong Hiep Nguyen, February 2014
    ! ACO_run performs all the required routines for a single run of ACO
       
 	use ACO_input
@@ -26,15 +26,18 @@
             ant(n_ant)%water_accumulated = 0.0
             do i=1,n_tree
                 do p = 1, n_sea
-                    sea_min_area(p) = sum(seasons(p)%min_crop_area(:))                    
+                    sea_min_area(p) = sum(seasons(p)%min_crop_area(:))
+                                        
                     ant(n_ant)%sea_cur_area(p) = 0
-                    ant(n_ant)%sea_cur_dryarea(p) = 0                    
+                    ant(n_ant)%sea_cur_dryarea(p) = 0
+                    
                     seasons(p)%wuse_crop(:) = 0
                     
                     do k = 1, n_crop(p)                 
                         ant(n_ant)%tree(i)%season(p)%crop(k)%max_area = seasons(p)%max_crop_area(k)
                         ant(n_ant)%tree(i)%season(p)%crop(k)%area_accumulated = 0
                         ant(n_ant)%tree(i)%season(p)%crop(k)%area_planted = 0
+                        ant(n_ant)%tree(i)%season(p)%crop(k)%dec_water(:) = 1
                     end do
                 end do
                 
@@ -43,13 +46,20 @@
                     bstatus = 0
                     wbstatus = 0
                     do p = 1, n_sea
-					    call prob_determination(n_it,aco_type,i,n_ant,q,1,p)	! sets probabilities for ant loop
-                        call prob_determination_wateruse(n_it,aco_type,i,n_ant,q,1,p)
-                    end do
-                    do p = 1, n_sea
+                        ant(n_ant)%tree(i)%season(p)%sea_status = 0
+                        ant(n_ant)%tree(i)%season(p)%max_status(:) = 0
+                        ant(n_ant)%tree(i)%season(p)%min_status(:) = 0
+                        ant(n_ant)%tree(i)%season(p)%crop(:)%water_status = 0
+
+                        call check_constraints(n_it,n_ant,i,q,1,p)	            ! check constraints
+                  
+					    call prob_determination(n_it,aco_type,i,n_ant,q,1,p)	! sets probabilities for ant loop 
 					    call path_selection(n_it,n_ant,i,q,1,p)				    ! calling path selection subroutine                  
-                        if (seasons(p)%name_crop(ant(n_ant)%tree(i)%season(p)%dec_crop(q)) /= "dryland") then                            
-                            call path_selection_wateruse(n_it,n_ant,i,q,1,p)                            
+                        if (seasons(p)%name_crop(ant(n_ant)%tree(i)%season(p)%dec_crop(q)) /= "dryland") then
+                            ant(n_ant)%tree(i)%season(p)%crop(ant(n_ant)%tree(i)%season(p)%dec_crop(q))%dec_water_status(:) = 0
+                            call constraints_avai_crop_water (n_it,n_ant,i,q,1,p)
+                            call prob_determination_wateruse(n_it,aco_type,i,n_ant,q,1,p)
+                            call path_selection_wateruse(n_it,n_ant,i,q,1,p)                       
                         end if
                     end do
                 end do
